45d70bffce8a25f251dc0c60261eaadb60025211
Sasha Peters <peteram1@sewanee.edu>
2019-05-28 17:18:39 -0500

    added index layout, next will add mongo connection and article saver

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+
+function scrapeNews() {
+    return $.ajax({
+        headers: {
+            "Content-Type": "application/json"
+        },
+        type: "GET",
+        url: "api/scraper"
+    }).then(function (res) {
+        console.log(res);
+        $(".container").append("<div class='list-group'>");
+        for(var i = 3; i<res.length; i++){
+            $(".container").append(
+                "<hr><br><a hrefclass='list-group-item list-group-item-action flex-column align-items-start'>" + 
+                "<div class='d-flex w-100 justify-content-between'>" + 
+                    "<h5 class='mb-1'>" + res[i].title + "</h5>" +
+                "</div>" +
+                "<small><a href='" + res[i].link + "'>Link to article</a></small><br>" +
+                "<small><button class='btn btn-outline-info my-2 my-sm-0' onclick='saveArticle(this)'>Save Article</button></small>" +
+                "<p class='mb-1'>" + res[i].description + "</p></a>"   
+          )
+        }
+    });
+}
