82909696e86a14d9761a8613ecbbf246c45405cb
Sasha Peters <peteram1@sewanee.edu>
2019-05-28 23:20:43 -0500

    finished hw

--- public/js/index.js
+++ public/js/index.js
@@ -11,14 +11,31 @@ function scrapeNews() {
         $(".container").append("<div class='list-group'>");
         for(var i = 3; i<res.length; i++){
             $(".container").append(
-                "<hr><br><a hrefclass='list-group-item list-group-item-action flex-column align-items-start'>" + 
-                "<div class='d-flex w-100 justify-content-between'>" + 
-                    "<h5 class='mb-1'>" + res[i].title + "</h5>" +
-                "</div>" +
+                "<hr><br><div><a hrefclass='list-group-item list-group-item-action flex-column align-items-start'>" + 
+                //"<div class='d-flex w-100 justify-content-between'>" + 
+                "<h5 class='mb-1'>" + res[i].title + "</h5>" +
+                //"</div>" +
                 "<small><a href='" + res[i].link + "'>Link to article</a></small><br>" +
                 "<small><button class='btn btn-outline-info my-2 my-sm-0' onclick='saveArticle(this)'>Save Article</button></small>" +
-                "<p class='mb-1'>" + res[i].description + "</p></a>"   
+                "<p class='mb-1'>" + res[i].description + "</p></a></div>"   
           )
         }
     });
+}
+
+function saveArticle(obj){
+    articleObj = {
+        title: $(obj).parent().parent().find("h5").text(),
+        description: $(obj).parent().parent().find("p").text(),
+        link: $(obj).parent().parent().find("small").find("a").attr("href")
+    }
+
+    $.ajax({
+        method: "POST",
+        url: "/saveArticle",
+        data: articleObj
+    }).then(function(data){
+        console.log(data);
+    })
+    console.log($(obj).parent().parent().find("small").find("a").attr("href"));
 }
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Require all models
-//var db = require("./models");
+var db = require("./models");
 
 var PORT = process.env.PORT || 8080;
 
@@ -64,16 +64,6 @@ app.get("/api/scraper", function(req, res) {
       result.link = "https://www.nytimes.com" + $(this).parent().parent().attr("href");
 
       articleArr.push(result);
-      // Create a new Article using the `result` object built from scraping
-      /*db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, log it
-          console.log(err);
-        });*/
     });
 
     // Send a message to the client
@@ -90,8 +80,9 @@ app.get("/saved", function(req, res) {
   // Grab every document in the Articles collection
   db.Article.find({})
     .then(function(dbArticle) {
+      console.log(dbArticle);
       // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
+      res.render("saved",dbArticle);
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
@@ -134,6 +125,77 @@ app.post("/articles/:id", function(req, res) {
       res.json(err);
     });
 });*/
+
+app.post("/saveArticle", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  console.log(req.body);
+  db.Article.create(req.body)
+    .then(function(dbArticle) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      console.log(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+    res.send("Scrape Complete");
+});
+
+app.delete("/deleteArticle",function(req,res){
+  console.log(req.body.id);
+  db.Article.deleteOne({ _id: req.body.id })
+    .then(function() { 
+      res.json("removed");
+    }).catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+app.delete("/deleteNote",function(req,res){
+  console.log(req.body.id);
+  db.Note.deleteOne({ _id: req.body.id })
+    .then(function() { 
+      res.json("removed note");
+    }).catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+app.get("/getComment/:id", function(req,res){
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      console.log(dbArticle);
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+app.post("/addComment/:id", function(req, res) {
+  console.log(req.body);
+  console.log(req.params.id);
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+    return db.Article.findOneAndModify({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+  })
+  .then(function(dbArticle) {
+    // If we were able to successfully update an Article, send it back to the client
+    res.json(dbArticle);
+  })
+  .catch(function(err) {
+    // If an error occurred, send it to the client
+    res.json(err);
+  });
+});
 
 // Start the server
 app.listen(PORT, function() {
